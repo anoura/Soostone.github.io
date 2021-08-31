30adf73f8a1063ffc7f2cd8e3ec738b06830e54f
Daniil Iaitskov <daniil.iaitskov@soostone.com>
2021-08-31 09:29:27 -0400

    Update 2ae8df7476b1371f16818dec06392eecd23f7123

--- offline-service-worker.js
+++ offline-service-worker.js
@@ -17,23 +17,27 @@ self.addEventListener("fetch", (event) => {
               });
           }
         }))
-    );
+  );
 });
 
 self.addEventListener('install', (event) => {
   console.log("Start install worker " + new Date());
   event.waitUntil(
-    caches.open("cache1").then(cache => {
-      return cache.addAll(
-        [ /* site.pages enumerates css and html */'/404.html','/getting-started/index.html',
-          '/getting-started/','/google-search/index.html',
-          '/google-search/','/haddock/index.html',
-          '/haddock/','/index.html',
-          '/','/assets/css/just-the-docs-dark.css','/assets/css/just-the-docs-default.css','/assets/css/just-the-docs-light.css','/assets/js/just-the-docs.js','/manifest.json','/offline-service-worker.js','/assets/css/style.css','/tutorial/index.html',
-          '/tutorial/','/user-manual/index.html',
-          '/user-manual/','/assets/js/search-data.json','/robots.txt',/* site.static_files enumerates images and js */'/assets/images/dark-background-waves.jpg','/assets/images/just-the-docs.png','/assets/images/logo-and-name.svg','/assets/images/logo-gem-100.png','/assets/images/search.svg','/assets/images/touch-icon/logo-120.png','/assets/images/touch-icon/logo-150.png','/assets/images/touch-icon/logo-152.png','/assets/images/touch-icon/logo-180.png','/assets/images/touch-icon/logo-196.png','/assets/images/touch-icon/logo-310.png','/assets/images/touch-icon/logo-70.png','/assets/images/touch-icon/logo-76.png','/assets/js/copy.js','/assets/js/vendor/lunr.min.js','/base.scss','/buttons.scss','/code.scss','/favicon.ico' /* fix trailing comma */
-        ]
-      );
-    })
+    caches.delete("cache1")
+      .then(ok => {
+        console.log("Previous cache is cleaned: " + ok);
+        caches.open("cache1").then(cache => {
+          return cache.addAll(
+            [ /* site.pages enumerates css and html */'/404.html','/getting-started/index.html',
+              '/getting-started/','/google-search/index.html',
+              '/google-search/','/haddock/index.html',
+              '/haddock/','/index.html',
+              '/','/assets/css/just-the-docs-dark.css','/assets/css/just-the-docs-default.css','/assets/css/just-the-docs-light.css','/assets/js/just-the-docs.js','/manifest.json','/offline-service-worker.js','/assets/css/style.css','/tutorial/index.html',
+              '/tutorial/','/user-manual/index.html',
+              '/user-manual/','/assets/js/search-data.json','/robots.txt',/* site.static_files enumerates images and js */'/assets/images/dark-background-waves.jpg','/assets/images/just-the-docs.png','/assets/images/logo-and-name.svg','/assets/images/logo-gem-100.png','/assets/images/search.svg','/assets/images/touch-icon/logo-120.png','/assets/images/touch-icon/logo-150.png','/assets/images/touch-icon/logo-152.png','/assets/images/touch-icon/logo-180.png','/assets/images/touch-icon/logo-196.png','/assets/images/touch-icon/logo-310.png','/assets/images/touch-icon/logo-70.png','/assets/images/touch-icon/logo-76.png','/assets/js/copy.js','/assets/js/vendor/lunr.min.js','/base.scss','/buttons.scss','/code.scss','/favicon.ico' /* fix trailing comma */
+            ]
+          );
+        });
+      })
   );
 });
